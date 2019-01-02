465080e9358480119e5339b84f0a8983418ee7cd
Piotr Mionskowski <piotr.mionskowski@gmail.com>
2019-01-03 00:09:09 +0100

    Add PUBLIC URL

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8701b8a5c49f7b82ea423333590fd979.js"
+  "https://miensol.pl/jsnes-web/precache-manifest.2ac08b38c6df7ffafc26dcd5fd844fa0.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("https://miensol.pl/jsnes-web/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
