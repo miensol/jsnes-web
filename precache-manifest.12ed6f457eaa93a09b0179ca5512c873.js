62b570c89b2ab665238641882f7778465ee32296
Piotr Mionskowski <piotr.mionskowski@gmail.com>
2019-01-03 00:24:30 +0100

    Home page

---
+++ precache-manifest.12ed6f457eaa93a09b0179ca5512c873.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "eabc7698d237000ef2ff",
+    "url": "/jsnes-web/static/js/runtime~main.eabc7698.js"
+  },
+  {
+    "revision": "9298790240581c9e4ee7",
+    "url": "/jsnes-web/static/js/main.92987902.chunk.js"
+  },
+  {
+    "revision": "3aa9ec14e8d2583cdca4",
+    "url": "/jsnes-web/static/js/1.3aa9ec14.chunk.js"
+  },
+  {
+    "revision": "9298790240581c9e4ee7",
+    "url": "/jsnes-web/static/css/main.c6b7ae6c.chunk.css"
+  },
+  {
+    "revision": "1033d004e56a001df25397d3e5ff811f",
+    "url": "/jsnes-web/index.html"
+  }
+];
