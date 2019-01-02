8d6dda2f2f8150c235b98596f02bd6478104bdf0
Piotr Mionskowski <piotr.mionskowski@gmail.com>
2019-01-03 00:11:58 +0100

    Add missing files

---
+++ precache-manifest.2ac08b38c6df7ffafc26dcd5fd844fa0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0e3e839b35df552e8b97",
+    "url": "https://miensol.pl/jsnes-web/static/js/runtime~main.0e3e839b.js"
+  },
+  {
+    "revision": "d22a9c7a945ddcad3502",
+    "url": "https://miensol.pl/jsnes-web/static/js/main.d22a9c7a.chunk.js"
+  },
+  {
+    "revision": "3aa9ec14e8d2583cdca4",
+    "url": "https://miensol.pl/jsnes-web/static/js/1.3aa9ec14.chunk.js"
+  },
+  {
+    "revision": "d22a9c7a945ddcad3502",
+    "url": "https://miensol.pl/jsnes-web/static/css/main.c6b7ae6c.chunk.css"
+  },
+  {
+    "revision": "a00a43c2117e5731eef67d48a3163ab3",
+    "url": "https://miensol.pl/jsnes-web/index.html"
+  }
+];
